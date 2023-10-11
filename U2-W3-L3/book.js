7ee7088b698355d611da5b67a6e46c4ced702426
Matteo Pucci <matteopuccifxgmail.com>
2023-10-11 20:47:57 +0200

    fix

--- U2-W3-L3/book.js
+++ U2-W3-L3/book.js
@@ -1,3 +1,8 @@
+const reset = (e) => {
+  const card = e.target.parentElement.parentElement;
+  card.remove();
+};
+
 fetch("https://striveschool-api.herokuapp.com/books")
   .then((response) => {
     if (response.ok) {
@@ -9,80 +14,67 @@ fetch("https://striveschool-api.herokuapp.com/books")
   .then((book) => {
     console.log("Lista di libri", book);
     const container = document.getElementById("container");
-
+    // CREO LA ROW
     let div = document.createElement("div");
     div.classList.add("row");
     container.appendChild(div);
-
+    // CREO TANTE COLONNE QUANTI I LIBRI E LE APPENDO ALLA ROW
     const arrayOfRows = Array.from(document.getElementsByClassName("row"));
+    //   SOLUZIONE MIGLIORE E PIU' VELOCE
     book.forEach((el) => {
-      let column = document.createElement("div");
-      column.classList.add("col-4");
+      const column = document.createElement("div");
+      column.classList.add("col-4", "mb-4");
+      const newCard = document.createElement("div");
+      newCard.classList.add("card", "h-100");
+      newCard.innerHTML = `<img src="${el.img}" class="card-img-top" alt="${el.title}">
+       <div class="card-body">
+          <h5 class="card-title">${el.title}</h5>
+          <p class="card-text">Prezzo: ${el.price} €</p>
+          <button class="btn btn-danger" onclick="reset(event)">Scarta</button>`;
+      column.appendChild(newCard);
       arrayOfRows[0].appendChild(column);
     });
-    const arrayOfColumns = Array.from(document.getElementsByClassName("col-4"));
-    console.log(arrayOfColumns);
 
-    arrayOfColumns.forEach((card) => {
-      book.forEach((el) => {
-        // console.log(card);
-        // console.log(card);
-        // console.log(el.img);
-        // console.log(el.title);
-        // console.log(el.price);
-        card.innerHTML = `
-            <div class="card">
-            <img src="${el.img}" class="card-img-top" alt="${el.title}">
-            <div class="card-body">
-            <h5 class="card-title">${el.title}</h5>
-            <p class="card-text">Prezzo: ${el.price} €</p>
-            <button class="btn btn-danger" onclick="reset">Scarta</button>
-            </div>
-            </div>
-            `;
-      });
-      // arrayOfColumns.forEach((col) => {
-      //   console.log(col);
-      // });
-    });
+    // SOLUZIONE GRAZIE A CHATGBPT
+
+    // const arrayOfColumns = Array.from(document.getElementsByClassName("col-4"));
+    // console.log(arrayOfColumns);
+
+    // book.forEach((el, index) => {
+    //   const columnIndex = index % arrayOfColumns.length; // Calcola l'indice della colonna utilizzando l'operatore modulo
+    //   const card = arrayOfColumns[columnIndex];
+
+    //   // Concatena il contenuto HTML per ogni libro alla colonna corrispondente
+    //   card.innerHTML += `
+    //       <div class="card">
+    //         <img src="${el.img}" class="card-img-top" alt="${el.title}">
+    //         <div class="card-body">
+    //           <h5 class="card-title">${el.title}</h5>
+    //           <p class="card-text">Prezzo: ${el.price} €</p>
+    //           <button class="btn btn-danger" onclick="reset()">Scarta</button>
+    //         </div>
+    //       </div>
+    //     `;
+    // });
+
+    // MIA SOLUZIONE
+
     // for (let i = 0; i < arrayOfColumns.length; i++) {
     //   for (let j = 0; j < book.length; j++) {
-    //     // console.log(card);
-    //     // console.log(card);
-    //     // console.log(el.img);
-    //     // console.log(el.title);
-    //     // console.log(el.price);
-    //     arrayOfColumns[i].innerHTML = `
-    //                   <div class="card">
-    //                     <img src="${book[j].img}" class="card-img-top" alt="${book[j].title}">
-    //                     <div class="card-body">
-    //                       <h5 class="card-title">${book[j].title}</h5>
-    //                       <p class="card-text">Prezzo: ${book[j].price} €</p>
-    //                       <button class="btn btn-danger" onclick="reset">Scarta</button>
-    //                     </div>
-    //                   </div>
-    //                 `;
+    //     if (i === j) {
+    //       arrayOfColumns[i].innerHTML = `
+    //       <div class="card h-100">
+    //          <img src="${book[j].img}" class="card-img-top h-75" alt="${book[j].title}">
+    //          <div class="card-body">
+    //            <h5 class="card-title">${book[j].title}</h5>
+    //          <p class="card-text">Prezzo: ${book[j].price} €</p>
+    //           <button class="btn btn-danger" onclick="reset()">Scarta</button>
+    //         </div>
+    //        </div>
+    //      `;
+    //     }
     //   }
     // }
-    // arrayOfColumns.forEach((card) => {
-    //   for (let i = 0; i < book.length; i++) {
-    //     // console.log(card);
-    //     // console.log(card);
-    //     // console.log(el.img);
-    //     // console.log(el.title);
-    //     // console.log(el.price);
-    //     card.innerHTML = `
-    //           <div class="card">
-    //             <img src="${book[i].img}" class="card-img-top" alt="${book[i].title}">
-    //             <div class="card-body">
-    //               <h5 class="card-title">${book[i].title}</h5>
-    //               <p class="card-text">Prezzo: ${book[i].price} €</p>
-    //               <button class="btn btn-danger" onclick="reset">Scarta</button>
-    //             </div>
-    //           </div>
-    //         `;
-    //   }
-    // });
   })
   .catch((err) => {
     console.log("Errore", err);
